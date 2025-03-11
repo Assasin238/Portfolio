87bcf07858554d1de0ad44d1432397dda7d37eab
Assasin238238 <harnach2006@gmail.com>
2025-03-11 19:19:37 +0100

    udělán funkční button na světlý a tmavý režim; odebrán zbytečný text na neexistující class

--- scripts.js
+++ scripts.js
@@ -50,7 +50,25 @@ const image = () =>{
 }
 
 const background = () => {
-    
+    let darkmode = localStorage.getItem('darkmode');
+    const background = document.getElementById('background');
+
+    const enableDarkmode = () => {
+        document.body.classList.add('darkmode')
+        localStorage.setItem('darkmode', 'active')
+    }
+
+    const disableDarkmode = () => {
+        document.body.classList.remove('darkmode')
+        localStorage.setItem('darkmode', null)
+    }
+
+    if (darkmode === "active") enableDarkmode()
+
+    background.addEventListener("click", () =>{
+        darkmode = localStorage.getItem('darkmode')
+        darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+    })
 }
 
 // toggle = !toggle;
